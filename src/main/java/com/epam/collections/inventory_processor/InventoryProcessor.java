2dd9bcd707e05b1a4948874c5ed0b3c7388295de
Alexey Perov <external.java.courses@gmail.com>
2019-10-22 00:03:18 +0300

    Initial commit

---
+++ src/main/java/com/epam/collections/inventory_processor/InventoryProcessor.java
@@ -0,0 +1,21 @@
+package com.epam.collections.inventory_processor;
+
+import java.util.List;
+
+/**
+ * Class for working with inventory
+ */
+public class InventoryProcessor {
+
+    public static void main(String[] args) {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+
+    public List<Product> sort(List<Product> products) {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+
+    public List<Product> distinct(List<Product> products) {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+}
---
+++ src/main/java/com/epam/collections/inventory_processor/Product.java
@@ -0,0 +1,11 @@
+package com.epam.collections.inventory_processor;
+
+/**
+ * Class that stores data about product
+ */
+public class Product {
+
+    public Product(int code, String title) {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+}
